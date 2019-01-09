5b2ca235e04e4c996df1501ac675c08698c5106c
Thao Thu Nguyen <thaothung.04@gmail.com>
2019-01-09 17:27:29 +0700

    add jquery scroll and resize, display and disappear menu

--- 02/1.js
+++ 02/1.js
@@ -1,2 +1,41 @@
-﻿
- 
+﻿$(document).ready(function () {
+    
+   
+    $(window).resize(function () { 
+        var h = $(window).height();
+        $('.intro').css({'height':h});
+    });
+
+    $('.fixed-menu').click(function () { 
+       $('.menudoc').addClass('appear');
+        
+    });
+      $('.close-btn').click(function () { 
+            $('.menudoc').removeClass('appear');
+             
+         });
+     $('.home').click(function () { 
+        $('html,body').animate({scrollTop : $('.intro').offset().top});
+        $('.menudoc').removeClass('appear');
+             
+     });
+     $('.contact').click(function () { 
+        $('html,body').animate({scrollTop : $('.link-contact').offset().top});
+        $('.menudoc').removeClass('appear');
+             
+     });
+     $('.services').click(function () { 
+        $('html').animate({scrollTop : $('.service-block').offset().top});
+        $('.menudoc').removeClass('appear');
+             
+     });
+     $('.about').click(function () { 
+        $('.menudoc').removeClass('appear');
+             
+        
+        $("HTML, BODY").animate({scrollTop:$(".text-intro").offset().top});
+     
+        
+        
+     });
+});
