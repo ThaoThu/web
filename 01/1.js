82268a75e6b90015abea0ca2372f82b92bdc6d2c
Thao Thu Nguyen <thaothung.04@gmail.com>
2019-01-09 15:53:57 +0700

    web01-landingPage

---
+++ 01/1.js
@@ -0,0 +1,24 @@
+﻿$().ready(function () {
+    $(window).resize(function() {
+        if ($(window).width() < 992) {
+            $('#main-menu').addClass('container-fluid');
+            $('#main-menu').removeClass('container');
+        }
+       else {
+        $('#main-menu').removeClass('container-fluid');
+        $('#main-menu').addClass('container');
+       }
+      });
+
+      //scroll
+      $('.nav-item:nth-child(2)').click(function(){
+        $("HTML, BODY").animate({scrollTop:$("#services").offset().top});
+        return false;
+      })
+      $('.nav-item:nth-child(3)').click(function(){
+        $("HTML, BODY").animate({scrollTop:$("#Contact").offset().top});
+        return false;
+      })
+    
+
+});
